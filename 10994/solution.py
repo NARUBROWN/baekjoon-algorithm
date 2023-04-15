da8af0fd132e1e7af884aa66931ebca2b4bf67bf
naru <rlck1998@naver.com>
2023-04-15 15:57:31 +0900

    별 찍기 - 19 도전

---
+++ 10994/solution.py
@@ -0,0 +1,24 @@
+n = int(input())
+board = [[" " for _ in range(4 * n - 3)] for _ in range(4 * n - 3)]
+
+
+def fill_star(n, x, y):
+    if n == 1:
+        board[y][x] = '*'
+        return
+
+    length = 4 * n - 3
+
+    for i in range(length):
+        board[y][x + i] = '*'
+        board[y + i][x] = '*'
+        board[y + length - 1][x + i] = '*'
+        board[y + i][x + length - 1] = '*'
+
+    fill_star(n - 1, x + 2, y + 2)
+
+
+fill_star(n, 0, 0)
+
+for i in board:
+    print(''.join(i))
