96ac15dbfe5e24103f85196c43844a8a40f19241
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-04-17 13:28:47 +0900

    별 찍기 - 19

--- 10994/solution.py
+++ 10994/solution.py
@@ -2,23 +2,22 @@ n = int(input())
 board = [[" " for _ in range(4 * n - 3)] for _ in range(4 * n - 3)]
 
 
-def fill_star(n, x, y):
+def draw_star(n, x, y):
     if n == 1:
-        board[y][x] = '*'
+        board[y][x] = "*"
         return
 
     length = 4 * n - 3
 
     for i in range(length):
-        board[y][x + i] = '*'
-        board[y + i][x] = '*'
-        board[y + length - 1][x + i] = '*'
-        board[y + i][x + length - 1] = '*'
+        board[y][x+i] = '*'
+        board[y+i][x] = '*'
+        board[y + length - 1][x+i] = "*"
+        board[y+i][x + length - 1] = "*"
 
-    fill_star(n - 1, x + 2, y + 2)
+    draw_star(n-1, x+2, y+2)
 
-
-fill_star(n, 0, 0)
+draw_star(n, 0, 0)
 
 for i in board:
     print(''.join(i))
